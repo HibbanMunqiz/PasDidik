99f793f7069d9125a81dade08e77f83f57037e7e
aca23mhm <mhmmohdabduh1@sheffield.ac.uk>
2025-08-06 16:36:31 +0800

    deleted tsx code for intro.mdx, insert menambah pelajar in tab format for pelajar.mdx and changed the layout of preview to advocate for bigger screens

--- src/components/preview.tsx
+++ src/components/preview.tsx
@@ -1,4 +1,5 @@
 'use client';
+
 import { cn } from '@/lib/utils';
 import React from 'react';
 
@@ -8,7 +9,24 @@ export const Preview = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <div
     className={cn(
-      `p-4 w-full max-w-md sm:max-w-3xl mx-auto rounded-lg border border-border bg-muted text-card-foreground`,
+      `
+        w-full
+        max-w-md
+        sm:max-w-3xl
+        lg:max-w-5xl
+        xl:max-w-screen-xl
+        min-h-[330px]
+        md:min-h-[400px]
+        lg:min-h-[500px]
+        xl:min-h-[600px]
+        p-5
+        mx-auto
+        rounded-lg
+        border
+        border-border
+        bg-muted
+        text-card-foreground
+      `,
       className
     )}
     ref={ref}
