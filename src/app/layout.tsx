d3146969c5fdb5cee64d632115046432f09b4835
aca23mhm <mhmmohdabduh1@sheffield.ac.uk>
2025-08-12 11:39:00 +0800

    added chatbot

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import './globals.css';
 import { ThemeProvider } from '@/components/theme-provider';
 import { Metadata } from 'next';
 import { meta } from '../../config/meta';
+import Script from 'next/script';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -32,6 +33,33 @@ export default function RootLayout({
         >
           {children}
         </ThemeProvider>
+
+        {/* Chatbase Script */}
+        <Script id="chatbase-script" strategy="afterInteractive">
+            {`(function(){
+              if(!window.chatbase||window.chatbase("getState")!=="initialized"){
+                window.chatbase=(...arguments)=>{
+                  if(!window.chatbase.q){window.chatbase.q=[]}
+                  window.chatbase.q.push(arguments)
+                };
+                window.chatbase=new Proxy(window.chatbase,{
+                  get(target,prop){
+                    if(prop==="q"){return target.q}
+                    return(...args)=>target(prop,...args)
+                  }
+                })
+              }
+              const onLoad=function(){
+                const script=document.createElement("script");
+                script.src="https://www.chatbase.co/embed.min.js";
+                script.id="sQZMOIOlzb1M23ryjjro_";
+                script.domain="www.chatbase.co";
+                document.body.appendChild(script)
+              };
+              if(document.readyState==="complete"){onLoad()}
+              else{window.addEventListener("load",onLoad)}
+            })();`}
+        </Script>
       </body>
     </html>
   );
